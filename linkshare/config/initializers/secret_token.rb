f15f0b2f9a7400f049409a728baab4a7b32e5627
toppy42 <toppy42@gmail.com>
2013-12-03 11:22:00 -0500

    rails init

---
+++ linkshare/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Linkshare::Application.config.secret_key_base = '54851b0132fe02bad678b5f1bc64dc88755d56075bb4372aa04b2f336a3b70ad7452ecf24dbe1e69d1b5c1097638f886da5fec1db8f58912ae6f369228611c6d'
---
+++ linkshare/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Linkshare::Application.config.session_store :cookie_store, key: '_linkshare_session'
