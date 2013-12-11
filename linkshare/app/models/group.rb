3efc5fe2f7b5ce9a60e5a2f902dfcd98cbe49f2e
jubrad <bradfield.justin7@gmail.com>
2013-12-10 20:25:23 -0500

    added functionality to test if link exists, and verification for personal gropus

--- linkshare/app/models/group.rb
+++ linkshare/app/models/group.rb
@@ -6,4 +6,6 @@ class Group < ActiveRecord::Base
 
   validates_uniqueness_of :name
   # function to get X number of popular groups
+
+
 end
--- linkshare/app/models/share.rb
+++ linkshare/app/models/share.rb
@@ -6,6 +6,16 @@ class Share < ActiveRecord::Base
 
 accepts_nested_attributes_for :link
 
+before_save :authed
+
+
+
+def authed
+	if !self.group.isprivate
+	elsif self.user.group_id != self.group.id
+		throw error
+	end
+end
   #has_many_and_belongs_to :tags
 
   # upvote function
